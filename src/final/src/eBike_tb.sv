847c2ace5723cd96036315de7a885bc8ac62171a
Madhav Rathi <mrathi3@tux-104.cae.wisc.edu>
2022-04-26 12:10:59 -0500

    Project provided files in final/src/

---
+++ src/final/src/eBike_tb.sv
@@ -0,0 +1,82 @@
+module eBike_tb();
+ 
+  // include or import tasks?
+
+  localparam FAST_SIM = 1;		// accelerate simulation by default
+
+  ///////////////////////////
+  // Stimulus of type reg //
+  /////////////////////////
+  reg clk,RST_n;
+  reg [11:0] BATT;				// analog values
+  reg [11:0] BRAKE,TORQUE;		// analog values
+  reg tgglMd;					// push button for assist mode
+  reg [15:0] YAW_RT;			// models angular rate of incline (+ => uphill)
+
+
+  //////////////////////////////////////////////////
+  // Declare any internal signal to interconnect //
+  ////////////////////////////////////////////////
+  wire A2D_SS_n,A2D_MOSI,A2D_SCLK,A2D_MISO;
+  wire highGrn,lowGrn,highYlw,lowYlw,highBlu,lowBlu;
+  wire hallGrn,hallBlu,hallYlw;
+  wire inertSS_n,inertSCLK,inertMISO,inertMOSI,inertINT;
+  wire cadence;
+  wire [1:0] LED;			// hook to setting from PB_intf
+  
+  wire signed [11:0] coilGY,coilYB,coilBG;
+  logic [11:0] curr;		// comes from hub_wheel_model
+  wire [11:0] BATT_TX, TORQUE_TX, CURR_TX;
+  logic vld_TX;
+  
+  //////////////////////////////////////////////////
+  // Instantiate model of analog input circuitry //
+  ////////////////////////////////////////////////
+  AnalogModel iANLG(.clk(clk),.rst_n(RST_n),.SS_n(A2D_SS_n),.SCLK(A2D_SCLK),
+                    .MISO(A2D_MISO),.MOSI(A2D_MOSI),.BATT(BATT),
+		    .CURR(curr),.BRAKE(BRAKE),.TORQUE(TORQUE));
+
+  ////////////////////////////////////////////////////////////////
+  // Instantiate model inertial sensor used to measure incline //
+  //////////////////////////////////////////////////////////////
+  eBikePhysics iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(inertSS_n),.SCLK(inertSCLK),
+	             .MISO(inertMISO),.MOSI(inertMOSI),.INT(inertINT),
+		     .yaw_rt(YAW_RT),.highGrn(highGrn),.lowGrn(lowGrn),
+		     .highYlw(highYlw),.lowYlw(lowYlw),.highBlu(highBlu),
+		     .lowBlu(lowBlu),.hallGrn(hallGrn),.hallYlw(hallYlw),
+		     .hallBlu(hallBlu),.avg_curr(curr));
+
+  //////////////////////
+  // Instantiate DUT //
+  ////////////////////
+  eBike #(FAST_SIM) iDUT(.clk(clk),.RST_n(RST_n),.A2D_SS_n(A2D_SS_n),.A2D_MOSI(A2D_MOSI),
+                         .A2D_SCLK(A2D_SCLK),.A2D_MISO(A2D_MISO),.hallGrn(hallGrn),
+			 .hallYlw(hallYlw),.hallBlu(hallBlu),.highGrn(highGrn),
+			 .lowGrn(lowGrn),.highYlw(highYlw),.lowYlw(lowYlw),
+			 .highBlu(highBlu),.lowBlu(lowBlu),.inertSS_n(inertSS_n),
+			 .inertSCLK(inertSCLK),.inertMOSI(inertMOSI),
+			 .inertMISO(inertMISO),.inertINT(inertINT),
+			 .cadence(cadence),.tgglMd(tgglMd),.TX(TX_RX),
+			 .LED(LED));
+			 
+			 
+  ////////////////////////////////////////////////////////////
+  // Instantiate UART_rcv or some other telemetry monitor? //
+  //////////////////////////////////////////////////////////
+			 
+  initial begin
+    <your magic here>
+	
+  end
+  
+  ///////////////////
+  // Generate clk //
+  /////////////////
+  always
+    #10 clk = ~clk;
+
+  ///////////////////////////////////////////
+  // Block for cadence signal generation? //
+  /////////////////////////////////////////
+	
+endmodule
