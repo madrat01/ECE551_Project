01722a596c4fcfe7ea607c260c1a6f2fc866ec9b
Alok Ballal <aballal@tux-161.cae.wisc.edu>
2022-05-01 19:34:24 -0500

    TB updates

--- src/final/src/eBike_tb.sv
+++ src/final/src/eBike_tb.sv
@@ -26,9 +26,14 @@ wire [1:0] LED;			// hook to setting from PB_intf
 
 wire signed [11:0] coilGY,coilYB,coilBG;
 logic [11:0] curr;		// comes from hub_wheel_model
-wire [11:0] BATT_TX, TORQUE_TX, CURR_TX;
+logic [11:0] BATT_TX, TORQUE_TX, CURR_TX;
 logic vld_TX;
 
+logic cadence_disable, cadence_increase;
+logic [11:0] avg_curr_prev;
+logic [19:0] omega_prev;
+logic TX_RX, rdy;
+logic [7:0] rx_data;
 //////////////////////////////////////////////////
 // Instantiate model of analog input circuitry //
 ////////////////////////////////////////////////
@@ -49,7 +54,17 @@ eBikePhysics iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(inertSS_n),.SCLK(inertSCLK),
 //////////////////////
 // Instantiate DUT //
 ////////////////////
-eBike #(FAST_SIM) iDUT(.clk(clk),.RST_n(RST_n),.A2D_SS_n(A2D_SS_n),.A2D_MOSI(A2D_MOSI),
+//eBike #(FAST_SIM) iDUT(.clk(clk),.RST_n(RST_n),.A2D_SS_n(A2D_SS_n),.A2D_MOSI(A2D_MOSI),
+//                       .A2D_SCLK(A2D_SCLK),.A2D_MISO(A2D_MISO),.hallGrn(hallGrn),
+//	 .hallYlw(hallYlw),.hallBlu(hallBlu),.highGrn(highGrn),
+//	 .lowGrn(lowGrn),.highYlw(highYlw),.lowYlw(lowYlw),
+//	 .highBlu(highBlu),.lowBlu(lowBlu),.inertSS_n(inertSS_n),
+//	 .inertSCLK(inertSCLK),.inertMOSI(inertMOSI),
+//	 .inertMISO(inertMISO),.inertINT(inertINT),
+//	 .cadence(cadence),.tgglMd(tgglMd),.TX(TX_RX),
+//	 .LED(LED));
+
+eBike iDUT(.clk(clk),.RST_n(RST_n),.A2D_SS_n(A2D_SS_n),.A2D_MOSI(A2D_MOSI),
                        .A2D_SCLK(A2D_SCLK),.A2D_MISO(A2D_MISO),.hallGrn(hallGrn),
 	 .hallYlw(hallYlw),.hallBlu(hallBlu),.highGrn(highGrn),
 	 .lowGrn(lowGrn),.highYlw(highYlw),.lowYlw(lowYlw),
@@ -57,42 +72,252 @@ eBike #(FAST_SIM) iDUT(.clk(clk),.RST_n(RST_n),.A2D_SS_n(A2D_SS_n),.A2D_MOSI(A2D
 	 .inertSCLK(inertSCLK),.inertMOSI(inertMOSI),
 	 .inertMISO(inertMISO),.inertINT(inertINT),
 	 .cadence(cadence),.tgglMd(tgglMd),.TX(TX_RX),
-	 .LED(LED));
-	 
+	 .LED(LED));	 
 	 
 ////////////////////////////////////////////////////////////
 // Instantiate UART_rcv or some other telemetry monitor? //
 //////////////////////////////////////////////////////////
+UART_rcv UART_rcv_tb(.clk(clk), .rst_n(RST_n), .RX(TX_RX), .rdy(rdy), .rx_data(rx_data), .clr_rdy(rdy));
 	 
 initial begin
 //<your magic here>
-  clk = 0;
-  RST_n = 0;
-  @ (posedge clk);
-  TORQUE = 12'h0;
-  BATT = 12'h0;
-  BRAKE = 12'h0;
-  YAW_RT = 16'h0;
-  tgglMd = 1'b0;
-  @ (posedge clk);
-  @ (negedge clk);
-  RST_n = 1;
-  cadence = 1;
-  @ (posedge clk);
-  TORQUE = 12'h700;
-  BATT = 12'hAC0;
-  BRAKE = 12'hFFF;
-  YAW_RT = 16'h1000;
-  repeat (2000000) @ (posedge clk);
-  TORQUE = 12'h500;
-  repeat (2000000) @ (posedge clk);
-  $stop();
+cadence_increase = 0;
+cadence_disable = 0;
+clk = 0;
+RST_n = 0;
+TORQUE = 12'h0;
+BATT = 12'hFFF;
+BRAKE = 12'hFFF;
+YAW_RT = 16'h0;
+tgglMd = 1'b0;
+cadence = 1;
+@(posedge clk);
+@(negedge clk);
+RST_n = 1; 	
+TORQUE = 12'h700;
+repeat(2000000) @(posedge clk);
+//TEST 1: When battery drops below threshold the error should be zero
+BATT = 12'h0;
+repeat(2000000) @(posedge clk);
+if(iDUT.sensorCondition.error !== 0) begin
+	$display("ERROR: Error signal doesnt drop down to 0 when Battery falls below threshold A98, Error %h", iDUT.sensorCondition.error);
+	$stop();
+end
+//Increasing the battery beyond threshold so that the error is no longer zero
+BATT = 12'hFFF;
+repeat(2000000) @(posedge clk);
+if(iDUT.sensorCondition.error === 0) begin
+	$display("ERROR: Error signal is 0 when Battery goes above the threshold A98, Error %h", iDUT.sensorCondition.error);
+	$stop();
+end
+
+//TEST 2: When Torque reduces the Avg current and omega should decrease
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+TORQUE = 12'h500;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev < iDUT.sensorCondition.avg_curr || omega_prev < iPHYS.omega) begin
+	$display("ERROR: With the decrease in torque the average current and omega doesn't go down, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+//When Torque increases the Avg current and omega should increase
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+TORQUE = 12'h700;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev > iDUT.sensorCondition.avg_curr || omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the increase in torque the average current and omega doesn't go up, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+
+//TEST 3: Increasing the cadence to check the increase in omega and average current
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+cadence_increase = 1;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev > iDUT.sensorCondition.avg_curr || omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the increase in cadence the average current and omega doesn't go up, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+//Decreasing the cadence to check the decrease in omega and average current
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+cadence_increase = 0;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev < iDUT.sensorCondition.avg_curr || omega_prev < iPHYS.omega) begin
+	$display("ERROR: With the decrease in cadence the average current and omega doesn't go down, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+
+//TEST 4: BRAKE testing
+//Appplying brake should reduce the average current and omega
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = 16'h40; //Low value of Omega
+BRAKE = 12'h000;
+repeat(2000000) @(posedge clk);
+if(iDUT.sensorCondition.avg_curr !== 0 || omega_prev < iPHYS.omega) begin
+	$display("ERROR: With the application of brake the average current and omega doesn't go down, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+//Release of brake should increase the average current and omega
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+BRAKE = 12'hFFF;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev > iDUT.sensorCondition.avg_curr || omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the release of brake the average current and omega doesn't go up, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+
+//TEST 5: Incline Testing
+//Reducing YAW should reduce the Average current and omega i.e. we are going downhill so less assist
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+YAW_RT = 16'hDEAD;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev < iDUT.sensorCondition.avg_curr || omega_prev < iPHYS.omega) begin
+	$display("ERROR: With the decrease in YAW(Downhill) the average current and omega doesn't go down, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+//Increasing YAW should increase the average current and omega i.e. we are going uphill and rider requires assist
+avg_curr_prev = iDUT.sensorCondition.avg_curr;
+omega_prev = iPHYS.omega;
+YAW_RT = 16'h7FFF;
+repeat(2000000) @(posedge clk);
+if(avg_curr_prev > iDUT.sensorCondition.avg_curr || omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the increase in YAW(Uphill) the average current and omega doesn't go up, Omega %h Prev_omega %h Avg Curr %h Prev_Avg_Curr %h  %t", iPHYS.omega, omega_prev, iDUT.sensorCondition.avg_curr, avg_curr_prev, $time);
+	$stop();	
+end
+YAW_RT = 16'h0;
+repeat(2000000) @(posedge clk);
+
+//TEST 6: Chaging the assist setting based on rider preference - Default: Medium Assist 
+//Toggle tgglMd to move to high assist which should increase the omega
+omega_prev = iPHYS.omega;
+@(posedge clk)
+tgglMd = 1'b1;
+@(posedge clk)
+tgglMd = 1'b0;
+repeat(2000000) @(posedge clk);
+if(omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the Rider's selction of high assist the omega doesn't go up, Omega %h Prev_omega %h %t", iPHYS.omega, omega_prev, $time);
+	$stop();	
+end
+
+//Toggle tgglMd to move to low assist which should decrease the omega
+omega_prev = iPHYS.omega;
+@(posedge clk)
+tgglMd = 1'b1;
+@(posedge clk)
+tgglMd = 1'b0;
+repeat(2000000) @(posedge clk);
+if(omega_prev < iPHYS.omega) begin
+	$display("ERROR: With the Rider's selction of no assist omega doesn't go down, Omega %h Prev_omega %h %t", iPHYS.omega, omega_prev, $time);
+	$stop();	
+end
+
+//Toggle tgglMd to move to low assist from no assist which should increase omega
+omega_prev = iPHYS.omega;
+@(posedge clk)
+tgglMd = 1'b1;
+@(posedge clk)
+tgglMd = 1'b0;
+repeat(2000000) @(posedge clk);
+if(omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the Rider's selction of low assist from no assist omega doesn't go up, Omega %h Prev_omega %h %t", iPHYS.omega, omega_prev, $time);
+	$stop();	
+end
+
+//Toggle tgglMd to move to Medium assist (default) from low assist which should increase omega
+omega_prev = iPHYS.omega;
+@(posedge clk)
+tgglMd = 1'b1;
+@(posedge clk)
+tgglMd = 1'b0;
+repeat(2000000) @(posedge clk);
+if(omega_prev > iPHYS.omega) begin
+	$display("ERROR: With the Rider's selction of medium assist from low assist omega doesn't go up, Omega %h Prev_omega %h %t", iPHYS.omega, omega_prev, $time);
+	$stop();	
+end
+
+//TEST 7: Telemetry test - Check if this can be run all the time in parallel
+@(posedge iDUT.sensorCondition.telemetry.trmt);
+@(posedge rdy);
+if(rx_data !== 8'haa)		// Check for delim1 data at UART reciever
+begin
+	$display("ERROR: Deliml data does not match. Expected data 0xaa Simulated data %h", rx_data);
+	$stop();
+end
+
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering delim2 data
+if(rx_data !== 8'h55)		// Check for delim2 data at UART reciever
+begin
+	$display("ERROR: Delim2 data does not match. Expected data 0x55 Simulated data %h", rx_data);
+	$stop();
+end
+
+@(posedge iDUT.sensorCondition.telemetry.trmt);
+BATT_TX = iDUT.sensorCondition.batt;
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering payload1 data
+if(rx_data !== {4'b0000,BATT_TX[11:8]})		// Check for payload1 data at UART reciever
+begin
+	$display("ERROR: Payload1 data does not match. Expected data %h Simulated data %h", BATT_TX[11:8], rx_data);
+	$stop();
+end
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering payload2 data
+if(rx_data !== BATT_TX[7:0])		// Check for payload2 data at UART reciever
+begin
+	$display("ERROR: Payload2 data does not match. Expected data %h Simulated data %h", BATT_TX[7:0], rx_data);
+	$stop();
+end
+
+@(posedge iDUT.sensorCondition.telemetry.trmt);
+CURR_TX = iDUT.sensorCondition.avg_curr;
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering payload3 data
+if(rx_data !== {4'b0000,CURR_TX[11:8]})		// Check for payload3 data at UART reciever
+begin
+	$display("ERROR: Payload3 data does not match. Expected data %h Simulated data %h", CURR_TX[11:8], rx_data);
+	$stop();
+end
+@(posedge iDUT.sensorCondition.telemetry.trmt);
+CURR_TX = iDUT.sensorCondition.avg_curr;
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering payload4 data
+if(rx_data !== CURR_TX[7:0])		// Check for payload4 data at UART reciever
+begin
+	$display("ERROR: Payload4 data does not match. Expected data %h Simulated data %h %t", CURR_TX[7:0], rx_data, $time);
+	$stop();
+end
+
+@(posedge iDUT.sensorCondition.telemetry.trmt);
+TORQUE_TX = iDUT.sensorCondition.avg_torque;
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering payload3 data
+if(rx_data !== {4'b0000,TORQUE_TX[11:8]})		// Check for payload3 data at UART reciever
+begin
+	$display("ERROR: Payload5 data does not match. Expected data %h Simulated data %h", TORQUE_TX[11:8], rx_data);
+	$stop();
+end
+@(posedge rdy);			// Wait for rdy at UART reciever after transfering payload4 data
+if(rx_data !== TORQUE_TX[7:0])		// Check for payload4 data at UART reciever
+begin
+	$display("ERROR: Payload6 data does not match. Expected data %h Simulated data %h", TORQUE_TX[7:0], rx_data);
+	$stop();
+end
+
+repeat(2000000) @(posedge clk);
+$stop();
 end
 
 always begin
-  repeat (2048) @ (posedge clk);
-  //@ (negedge clk);
-  cadence = ~cadence;
+	if(cadence_increase)
+		repeat (1024) @ (posedge clk);
+	else
+		repeat (2048) @ (posedge clk);
+	//@ (negedge clk);
+	if(cadence_disable) begin
+		cadence = 0;
+	end else begin
+		cadence = ~cadence;
+	end
 end  
 
 ///////////////////
--- src/final/src/telemetry.sv
+++ src/final/src/telemetry.sv
@@ -1,90 +1,137 @@
-module telemetry (
-    input logic         clk,
-    input logic         rst_n,
-    input logic [11:0] 	batt_v,
-    input logic [11:0] 	avg_curr,
-    input logic [11:0] 	avg_torque,
-	output logic	   	TX
-);
+module telemetry(clk, rst_n, batt_v, avg_curr, avg_torque, TX);
+/////////////////////////////////////////////////////////////////
+// UART transmitter wrapper for periodic serial transfer of   //
+// battery voltage, motor current and input torque	     //
+//////////////////////////////////////////////////////////////
 
-typedef enum logic [3:0] {IDLE, DELIM1, DELIM2, PAYLOAD1, PAYLOAD2, PAYLOAD3, PAYLOAD4, PAYLOAD5, PAYLOAD6} tran_state_t;
+input clk, rst_n;
+input [11:0] batt_v, avg_curr, avg_torque;
 
-logic [19:0]        counter;
-tran_state_t        transmiterFSM, transmiterFSM_next, transmiterFSM_dly;
-logic               trmt;
-logic               tx_done, tx_done_dly, tx_done_rise;
-logic [7:0]         tx_data;
+output logic TX;
 
-// Counter to count till 20'hfe502
-// Since we need to send a payload every 47.68 times a second and our frequency is 50MHz
-always_ff @ (posedge clk or negedge rst_n) begin
-    if (~rst_n)
-        counter <= 20'b0;
-    else
-        counter <= (counter != 20'hfe502) ? counter + 1 : 20'h0;
+logic trmt, tx_done;
+logic [7:0] tx_data;
+logic [19:0] cnt;
+
+// States for the FSM - One of each byte to be transferred along with IDLE
+typedef enum reg [3:0] {IDLE,DELIM1,DELIM2,PAYLOAD1,PAYLOAD2,PAYLOAD3,PAYLOAD4,PAYLOAD5,PAYLOAD6} state_t;
+state_t state, nxt_state;
+
+// Instantiate the UART Transmitter
+UART_tx UARTTX(.clk(clk), .rst_n(rst_n), .TX(TX), .trmt(trmt), .tx_data(tx_data), .tx_done(tx_done));
+
+// Counter which resets to zero after 1041666 cycles which is equal to 1/48th of a second with clock at 50MHz
+always @(posedge clk, negedge rst_n)
+begin
+	if(!rst_n)
+		cnt <= 0;
+	else if(cnt == 20'hFE502)
+		cnt <= 0;
+	else
+		cnt <= cnt + 1;
 end
 
-// Move to next state next cycle
-always_ff @(posedge clk or negedge rst_n) begin : TRANS_FSM_NEXT
-    if (~rst_n)
-        transmiterFSM <= IDLE;
-    else
-        transmiterFSM <= transmiterFSM_next;
+//Telemetry state machine
+//State flop for the telemetry state machine
+always @(posedge clk,negedge rst_n) 
+begin 
+	if(!rst_n) 
+		state <= IDLE;
+	else 
+		state <= nxt_state;
 end
 
-always_ff @(posedge clk)
-	transmiterFSM_dly <= transmiterFSM;
+//Output and state transisition logic for telemetry state machine
+always_comb
+begin
+nxt_state = state;
+trmt = 0;
+tx_data = 0;
 
-// trmt high for a single cycle when state changed (and not changed to idle state)
-assign trmt = ((transmiterFSM != transmiterFSM_dly) && (transmiterFSM != IDLE));
+case(state)
 
-always_ff @ (posedge clk)
-	tx_done_dly <= tx_done;
-
-// tx_done is used to move to next state (but tx done is high for more than 1 cycle, so implement a rising edge detector)
-assign tx_done_rise = ~tx_done_dly & tx_done;
-
-// Move to transmit when counter reaches 20'hfe502 and next states when tx_done_rise detected
-always_comb begin : TRANS_FSM
-    tx_data = 8'h0;
-    transmiterFSM_next = transmiterFSM;
-    case (transmiterFSM)
-    IDLE : transmiterFSM_next = counter == 20'hfe502 ? DELIM1 : transmiterFSM_next;
-    DELIM1 : begin
-                 tx_data = 8'hAA; 
-                 if (tx_done_rise) transmiterFSM_next = DELIM2;
-                 end
-	DELIM2 : begin
-		  	  tx_data = 8'h55;
-		  	  if (tx_done_rise) transmiterFSM_next = PAYLOAD1;
-	          end
-    PAYLOAD1 : begin
-		   tx_data = {4'h0, batt_v[11:8]};
-		   if (tx_done_rise) transmiterFSM_next = PAYLOAD2;
-	   	   end
-	PAYLOAD2 : begin
-		   tx_data = batt_v[7:0];
-		   if (tx_done_rise) transmiterFSM_next = PAYLOAD3;
-	   	  end
-	PAYLOAD3 : begin
-		   tx_data = {4'h0, avg_curr[11:8]};
-		   if (tx_done_rise) transmiterFSM_next = PAYLOAD4;
-	   	   end
-	PAYLOAD4 : begin
-		   tx_data = avg_curr[7:0];
-		   if (tx_done_rise) transmiterFSM_next = PAYLOAD5;
-	  	   end
-	PAYLOAD5 : begin
-		   tx_data = {4'h0, avg_torque[11:8]};
-		   if (tx_done_rise) transmiterFSM_next = PAYLOAD6;
-	           end
-	PAYLOAD6 : begin
-		   tx_data = avg_torque[7:0];
-		   transmiterFSM_next = IDLE;
-	   	   end
-    endcase
+DELIM1: begin				//State to wait for serial transfer of DELIM1 to complete
+tx_data = 8'haa;
+	if(tx_done) begin		// Wait for tx_done to arrive from DELIM1 trasfer and load DELIM2 (0x55) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = DELIM2;
+		tx_data = 8'h55;
+		trmt = 1;
+	end
 end
 
-UART_tx uart_tx (.clk (clk), .rst_n (rst_n), .TX (TX), .trmt(trmt), .tx_data(tx_data), .tx_done(tx_done));
+DELIM2: begin				//State to wait for serial transfer of DELIM2 to complete
+tx_data = 8'h55;
+	if(tx_done) begin		// Wait for tx_done to arrive from DELIM2 trasfer and load payload1 (MSB nibble of Battery voltage) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = PAYLOAD1;
+		tx_data = {4'b0000,batt_v[11:8]};
+		trmt = 1;
+	end
+end
+
+PAYLOAD1: begin				//State to wait for serial transfer of payload1 to complete
+tx_data = {4'b0000,batt_v[11:8]};
+	if(tx_done) begin		// Wait for tx_done to arrive from payload1 trasfer and load payload2 (LSB byte of Battery voltage) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = PAYLOAD2;
+		tx_data = batt_v[7:0];
+		trmt = 1;
+	end
+end
+
+PAYLOAD2: begin				//State to wait for serial transfer of payload2 to complete
+tx_data = batt_v[7:0];
+	if(tx_done) begin		// Wait for tx_done to arrive from payload2 trasfer and load payload3 (MSB nibble of motor current) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = PAYLOAD3;
+		tx_data = {4'b0000,avg_curr[11:8]};
+		trmt = 1;
+	end
+end
+
+PAYLOAD3: begin				//State to wait for serial transfer of payload3 to complete
+tx_data = {4'b0000,avg_curr[11:8]};
+	if(tx_done) begin		// Wait for tx_done to arrive from payload3 trasfer and load payload2 (LSB byte of motor current) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = PAYLOAD4;
+		tx_data = avg_curr[7:0];
+		trmt = 1;
+	end
+end
+
+PAYLOAD4: begin				//State to wait for serial transfer of paylaod4 to complete
+tx_data = avg_curr[7:0];
+	if(tx_done) begin		// Wait for tx_done to arrive from payload4 trasfer and load payload5 (MSB nibble of input torque) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = PAYLOAD5;
+		tx_data = {4'b0000,avg_torque[11:8]};
+		trmt = 1;
+	end
+end
+
+PAYLOAD5: begin				//State to wait for serial transfer of paylaod5 to complete
+tx_data = {4'b0000,avg_torque[11:8]};
+	if(tx_done) begin		// Wait for tx_done to arrive from payload5 trasfer and load payload6 (LSB byte of input torque) in next clock cycle and assert trmt for one clock cycle
+		nxt_state = PAYLOAD6;
+		tx_data = avg_torque[7:0];
+		trmt = 1;
+	end
+end
+
+PAYLOAD6: begin				//State to wait for serial transfer of paylaod6 to complete
+tx_data = avg_torque[7:0];
+	if(tx_done)
+		nxt_state = IDLE;
+end
+
+default: begin				// Setting up IDLE as the default state
+	if(!rst_n) begin		// Assiging reset values to tx_data and trmt
+		tx_data = 8'h00;
+		trmt = 0;
+	end
+	else if(cnt == 20'h1) begin		// Moving to DELIM1 state and feeding 0xaa to UART_tx after every 1/48th of a second when counter resets to 0 and assert trmt for one clock cycle
+		nxt_state = DELIM1;
+		tx_data = 8'haa;
+		trmt = 1;
+	end
+end
+
+endcase
+end
 
 endmodule
